245ade328461524f8fec47b980eaa1fa9c92363c
Kolyadich <Kolyadich9@gmail.com>
2015-12-10 22:56:05 +0200

    my django create_app2

--- network/views.py
+++ network/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
